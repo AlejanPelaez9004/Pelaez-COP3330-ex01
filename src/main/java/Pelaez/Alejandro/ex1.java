91e523f8d70112e7da20fb056119c559640c7667
AlejanPelaez9004 <alejanpelaez@knights.ucf.edu>
2021-08-26 19:27:42 -0700

    Complete

---
+++ src/main/java/Pelaez/Alejandro/ex1.java
@@ -0,0 +1,12 @@
+package Pelaez.Alejandro;
+import java.util.Scanner;
+
+public class ex1 {
+    public static void main(String[] args)
+    {
+        System.out.print("What is your name?");
+        Scanner input = new Scanner(System.in);
+        String name = input.next();
+        System.out.print(name);
+    }
+}
